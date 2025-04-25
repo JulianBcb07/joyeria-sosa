9324eb2fe7407cb0371e73fe7350b359e4248cb3
Julian Daniel Bacab Chan <julianbacab.dc7@gmail.com>
2025-04-24 19:27:19 -0600

    update: backend iniciado

---
+++ server/routes/index.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { pool } from "../db.js";
+
+const router = Router();
+
+
+router.get('/ping', async (req, res) => {
+    const [rows] = await pool.query('SELECT * FROM users');
+    console.log(rows[0]);
+    res.json(rows[0]);
+})
+
+export default router;
