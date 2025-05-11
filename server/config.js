e0e7175cf47fa393d14809a3df0fb07c564614ac
Julian Daniel Bacab Chan <julianbacab.dc7@gmail.com>
2025-05-11 13:25:13 -0600

    update: variable de entorno backend

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
 // establecer el puerto de conexion
 export const PORT = process.env.PORT || 4000;
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+export const TOKEN_SECRET =
+  process.env.TOKEN_SECRET || "cR9H8vX2qWpLm3nB6Zt5wY7zA1dF4gJ0kEySfGhUjKl";
