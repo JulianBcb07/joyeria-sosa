962fb872e48b4a430fd2225c906cbb3eec4fa01b
Julian Daniel Bacab Chan <julianbacab.dc7@gmail.com>
2025-05-11 19:00:05 -0600

    update: backend request

--- server/index.js
+++ server/index.js
@@ -41,6 +41,11 @@ app.use(indexRoutes);
 app.use(routerLogin);
 app.use(categoryRoutes);
 app.use(productRoutes);
-app.listen(PORT);
 
+// 3. Finalmente, maneja todas las demás rutas con el frontend
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
+
+app.listen(PORT);
 console.log(`server en el puerto ${PORT}`);
