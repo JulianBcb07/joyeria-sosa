30bc2d852836b88eedbaaf6b7af0d3c573217a38
Julian Daniel Bacab Chan <julianbacab.dc7@gmail.com>
2025-05-12 19:16:17 -0600

    update

--- server/config.js
+++ server/config.js
@@ -1,3 +1,10 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
 // establecer el puerto de conexion
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 export const PORT = process.env.PORT || 4000;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "fallback_secret";
--- server/db.js
+++ server/db.js
@@ -9,10 +9,14 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 console.log("Se está ejecutando db.js");
-dotenv.config({ path: "./.env" });
-dotenv.config();
 console.log("Leyendo .env...");
-console.log("Usuario DB:", process.env.DATABASE_USER);
+console.log("DB name:", process.env.DATABASE || process.env.DB_NAME);
+console.log("Usuario DB:", process.env.DATABASE_USER || process.env.DB_USER);
+console.log(
+  "password User DB:",
+  process.env.DATABASE_PASSWORD || process.env.DB_PASSWORD
+);
+console.log("DB HOST:", process.env.DATABASE_HOST || process.env.DB_HOST);
 
 // Importante leer la documentacion de express, mysql2 como se utiliza estos segmentos de codigo.
 // establecemos la conexion de la base de datos mysql
